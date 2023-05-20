c2962e5ce84aff89207d3ff89dca69fa8b50c2b5
Ba1tur <zbajtur@gmail.com>
2023-05-20 21:44:01 +0600

    first_page

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,12 @@
 import '@/styles/globals.css'
+import '../styles/MainSlider.css'
+import '../styles/Heart.css'
 import Layout from "@/components/Layout/Layout";
 
 export default function App({Component, pageProps}) {
     return (
-
         <Layout>
             <Component {...pageProps} />
         </Layout>
     )
-
-
 }
